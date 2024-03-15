7096a812683476856ccb85c5f560030e78d9a0f7
PriyabrataSahoo2022 <priyabrata.sahoo@erelego.com>
2024-03-15 17:38:09 +0530

    Update TokenCodeRepresentation.java

--- keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/providers/representations/TokenCodeRepresentation.java
+++ keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/providers/representations/TokenCodeRepresentation.java
@@ -3,8 +3,8 @@ package cc.coopersoft.keycloak.phone.providers.representations;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.jboss.logging.Logger;
 import org.keycloak.models.utils.KeycloakModelUtils;
-
 import java.security.SecureRandom;
 import java.util.Date;
 
@@ -20,22 +20,42 @@ public class TokenCodeRepresentation {
     private Date createdAt;
     private Date expiresAt;
     private Boolean confirmed;
+    
+    private static final Logger LOGGER = Logger.getLogger(TokenCodeRepresentation.class);
 
     public static TokenCodeRepresentation forPhoneNumber(String phoneNumber) {
+	
+	LOGGER.info("Inside forPhoneNumber");
 
         TokenCodeRepresentation tokenCode = new TokenCodeRepresentation();
 
         tokenCode.id = KeycloakModelUtils.generateId();
+        
         tokenCode.phoneNumber = phoneNumber;
+        
         tokenCode.code = generateTokenCode();
+        
         tokenCode.confirmed = false;
+        
+        LOGGER.debug("Code is - "+tokenCode.code); 
+        
+        LOGGER.debug("Token id is - "+tokenCode.id); 
 
         return tokenCode;
     }
 
     private static String generateTokenCode() {
-        SecureRandom secureRandom = new SecureRandom();
-        Integer code = secureRandom.nextInt(999_999);
-        return String.format("%06d", code);
+	
+	LOGGER.info("Inside generateTokenCode");
+        
+	SecureRandom secureRandom = new SecureRandom();
+        
+        Integer code = secureRandom.nextInt(99_99);
+
+        String tokenCode = String.format("%04d", code);
+        
+        LOGGER.debug("tokenCode :: "+tokenCode);
+        
+        return tokenCode;
     }
 }
