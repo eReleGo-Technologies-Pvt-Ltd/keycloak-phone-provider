173ea3c4a5ec1d950e66b90db349d79f3f67eb2a
Pradeep Reddy <121370077+PradeepReddy1996@users.noreply.github.com>
2024-12-16 14:33:34 +0530

    changes in the sms properties.

--- keycloak-sms-provider-munshify/src/main/java/cc/coopersoft/keycloak/phone/providers/sender/utils/SmsProperties.java
+++ keycloak-sms-provider-munshify/src/main/java/cc/coopersoft/keycloak/phone/providers/sender/utils/SmsProperties.java
@@ -10,16 +10,15 @@ public interface SmsProperties {
     
     public static final String EXPIRY_TIME_IN_MINS = "15";
     
-    public static final String ENTITY_ID = "1401431560000050930";
+    public static final String ENTITY_ID = "1701158107050838436"; //  "1401431560000050930";
     
-    public static final String TEMPLATE_ID = "1407166538256964841";
+    public static final String TEMPLATE_ID = "1707173432260079746";  //"1407166538256964841";
     
-    public static final String SENDER_ID = "MNSHFY";
+    public static final String SENDER_ID = "ERELGO"; //"MNSHFY";
     
     public static final String DEFAULT_EMAILS = "gulshanmohanty007@gmail.com,sukshithashetty1998@gmail.com";
     
     public static final String DEFAULT_MOBILE_NUMBERS = "8249210815,7624841503";
     
-    public static final String OTP_SMS_MSG = "Dear ${user}, ${otp} is the OTP to verify your mobile number on ${datetime}. OTP is valid for 15 minutes. DO NOT disclose it to anyone\n\n- Team Munshify";
-
+    public static final String OTP_SMS_MSG = "Dear ${user}, ${otp} is the OTP to verify your mobile number on ${datetime}. OTP is valid for 15 minutes. DO NOT disclose it to anyone\n\n- Team EreleGo"; //Team Munshify
 }
